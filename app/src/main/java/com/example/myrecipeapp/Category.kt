76536486d5b19e19054d1135500685921ec89a63
Sahil Yadav <raosahil15266@gmail.com>
2024-04-13 14:34:24 +0530

    Updated Documentation

--- app/src/main/java/com/example/myrecipeapp/Category.kt
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -3,9 +3,14 @@ package com.example.myrecipeapp
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-// Data class for storing category information
+/*
+id("kotlin-parcelize") - Dependency is used inside plugins in build.gradle.app
+@Parcelize helps to serialize and deserialize an object
+Serializing an object helps us to pass an object from one screen to another in a format that is used by our backStackEntry to pass objects
+And Deserializing an object makes our formatted object back into the object that we are passing to the next screen
+*/
 @Parcelize
-data class Category(
+data class Category(                // Data class for storing category information
     val idCategory: String,         // Category id
     val strCategory: String,        // Category name
     val strCategoryThumb: String,   // Category Thumbnail
